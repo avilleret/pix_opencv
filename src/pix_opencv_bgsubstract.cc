22339bb8a91d5d65f27b7a247dd5b93f28637bbc
Antoine Villeret <antoine.villeret@gmail.com>
2020-04-17 23:49:28 +0200

    WIP on bgsubstract

--- src/pix_opencv_bgsubstract.cc
+++ src/pix_opencv_bgsubstract.cc
@@ -1,21 +1,6 @@
-////////////////////////////////////////////////////////
-//
-// GEM - Graphics Environment for Multimedia
-//
-// zmoelnig@iem.kug.ac.at
-//
-// Implementation file
-//
-//    Copyright (c) 1997-2000 Mark Danks.
-//    Copyright (c) Günther Geiger.
-//    Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::für::umläute. IEM
-//    Copyright (c) 2002 James Tittle & Chris Clepper
-//    For information on usage and redistribution, and for a DISCLAIMER OF ALL
-//    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
-//
-/////////////////////////////////////////////////////////
-
 #include "pix_opencv_bgsubstract.h"
+#include "pix_opencv_utils.h"
+#include <opencv2/imgproc.hpp>
 
 CPPEXTERN_NEW(pix_opencv_bgsubstract)
 
@@ -49,179 +34,30 @@ pix_opencv_bgsubstract :: pix_opencv_bgsubstract()
 /////////////////////////////////////////////////////////
 pix_opencv_bgsubstract :: ~pix_opencv_bgsubstract()
 { 
-    	//Destroy cv_images to clean memory
-	cvReleaseImage(&orig);
-    	cvReleaseImage(&gray);
-    	cvReleaseImage(&rgb);
-    	cvReleaseImage(&prev_gray);
-    	cvReleaseImage(&grayLow);
-    	cvReleaseImage(&grayUp);
-    	cvReleaseImage(&diff_8U);
 }
 
 /////////////////////////////////////////////////////////
 // processImage
 //
 /////////////////////////////////////////////////////////
-void pix_opencv_bgsubstract :: processRGBAImage(imageStruct &image)
-{
-
-  if ((this->comp_xsize!=image.xsize)||(this->comp_ysize!=image.ysize)||(!orig)) {
-
-	this->comp_xsize = image.xsize;
-	this->comp_ysize = image.ysize;
-
-    	//Destroy cv_images to clean memory
-	cvReleaseImage(&orig);
-    	cvReleaseImage(&gray);
-    	cvReleaseImage(&rgb);
-    	cvReleaseImage(&prev_gray);
-    	cvReleaseImage(&grayLow);
-    	cvReleaseImage(&grayUp);
-    	cvReleaseImage(&diff_8U);
-
-	//create the orig image with new size
-        orig = cvCreateImage(cvSize(image.xsize,image.ysize), IPL_DEPTH_8U, 4);
-        rgb  = cvCreateImage(cvSize(image.xsize,image.ysize), IPL_DEPTH_8U, 3);
-
-    	// Create the output images with new sizes
-    	gray = cvCreateImage(cvSize(orig->width,orig->height), IPL_DEPTH_8U, 1);
-    	grayLow = cvCreateImage(cvSize(orig->width,orig->height), IPL_DEPTH_8U, 1);
-    	grayUp = cvCreateImage(cvSize(orig->width,orig->height), IPL_DEPTH_8U, 1);
-    	prev_gray = cvCreateImage(cvSize(orig->width,orig->height), 8, 1);
-    	diff_8U = cvCreateImage(cvSize(orig->width,orig->height), 8, 1);
-    }
-    // Here we make a copy of the pixel data from image to orig->imageData
-    // orig is a IplImage struct, the default image type in openCV, take a look on the IplImage data structure here
-    // http://www.cs.iit.edu/~agam/cs512/lect-notes/opencv-intro/opencv-intro.html 
-    memcpy( orig->imageData, image.data, image.xsize*image.ysize*4 );
-    
-    // Convert to grayscale
-    cvCvtColor(orig, gray, CV_BGRA2GRAY);
-
-    if (x_set) {
-    	memcpy( prev_gray->imageData, gray->imageData, image.xsize*image.ysize );
-	x_set=0;
-    } 
-
-    cvSubS (prev_gray,cvScalar(x_threshold,x_threshold,x_threshold,x_threshold),grayLow,NULL);
-    cvAddS (prev_gray,cvScalar(x_threshold,x_threshold,x_threshold,x_threshold),grayUp,NULL);
-    cvInRange (gray, grayLow, grayUp, diff_8U);
-
-    cvNot (diff_8U,diff_8U);
-
-    cvCvtColor(diff_8U, orig, CV_GRAY2BGRA);
-  
-    //copy back the processed frame to image
-    memcpy( image.data, orig->imageData, image.xsize*image.ysize*4 );
-}
-
-void pix_opencv_bgsubstract :: processRGBImage(imageStruct &image)
-{
-
-  if ((this->comp_xsize!=image.xsize)||(this->comp_ysize!=image.ysize)||(!orig)) {
-
-	this->comp_xsize = image.xsize;
-	this->comp_ysize = image.ysize;
-
-    	//Destroy cv_images to clean memory
-	cvReleaseImage(&orig);
-    	cvReleaseImage(&gray);
-    	cvReleaseImage(&rgb);
-    	cvReleaseImage(&prev_gray);
-    	cvReleaseImage(&grayLow);
-    	cvReleaseImage(&grayUp);
-    	cvReleaseImage(&diff_8U);
-
-	//create the orig image with new size
-        orig = cvCreateImage(cvSize(image.xsize,image.ysize), IPL_DEPTH_8U, 4);
-        rgb  = cvCreateImage(cvSize(image.xsize,image.ysize), IPL_DEPTH_8U, 3);
-
-    	// Create the output images with new sizes
-    	gray = cvCreateImage(cvSize(orig->width,orig->height), IPL_DEPTH_8U, 1);
-    	grayLow = cvCreateImage(cvSize(orig->width,orig->height), IPL_DEPTH_8U, 1);
-    	grayUp = cvCreateImage(cvSize(orig->width,orig->height), IPL_DEPTH_8U, 1);
-    	prev_gray = cvCreateImage(cvSize(orig->width,orig->height), 8, 1);
-    	diff_8U = cvCreateImage(cvSize(orig->width,orig->height), 8, 1);
-    }
-    // Here we make a copy of the pixel data from image to orig->imageData
-    // orig is a IplImage struct, the default image type in openCV, take a look on the IplImage data structure here
-    // http://www.cs.iit.edu/~agam/cs512/lect-notes/opencv-intro/opencv-intro.html 
-    memcpy( rgb->imageData, image.data, image.xsize*image.ysize*3 );
-    
-    // Convert to grayscale
-    cvCvtColor(rgb, gray, CV_BGRA2GRAY);
-
-    if (x_set) {
-    	memcpy( prev_gray->imageData, gray->imageData, image.xsize*image.ysize );
-	x_set=0;
-    } 
-
-    cvSubS (prev_gray,cvScalar(x_threshold,x_threshold,x_threshold,x_threshold),grayLow,NULL);
-    cvAddS (prev_gray,cvScalar(x_threshold,x_threshold,x_threshold,x_threshold),grayUp,NULL);
-    cvInRange (gray, grayLow, grayUp, diff_8U);
-
-    cvNot (diff_8U,diff_8U);
-
-    cvCvtColor(diff_8U, rgb, CV_GRAY2BGR);
-  
-    //copy back the processed frame to image
-    memcpy( image.data, rgb->imageData, image.xsize*image.ysize*3 );
-
-}
-
-void pix_opencv_bgsubstract :: processYUVImage(imageStruct &image)
-{
-  post( "pix_opencv_bgsubstract : yuv format not supported" );
-}
-    	
-void pix_opencv_bgsubstract :: processGrayImage(imageStruct &image)
+void pix_opencv_bgsubstract :: processImage(imageStruct &image)
 { 
-
-  if ((this->comp_xsize!=image.xsize)||(this->comp_ysize!=image.ysize)||(!orig)) {
-
-	this->comp_xsize = image.xsize;
-	this->comp_ysize = image.ysize;
-
-    	//Destroy cv_images to clean memory
-	cvReleaseImage(&orig);
-    	cvReleaseImage(&gray);
-    	cvReleaseImage(&rgb);
-    	cvReleaseImage(&prev_gray);
-    	cvReleaseImage(&grayLow);
-    	cvReleaseImage(&grayUp);
-    	cvReleaseImage(&diff_8U);
-
-	//create the orig image with new size
-        orig = cvCreateImage(cvSize(image.xsize,image.ysize), IPL_DEPTH_8U, 4);
-        rgb  = cvCreateImage(cvSize(image.xsize,image.ysize), IPL_DEPTH_8U, 3);
-
-    	// Create the output images with new sizes
-    	gray = cvCreateImage(cvSize(orig->width,orig->height), IPL_DEPTH_8U, 1);
-    	grayLow = cvCreateImage(cvSize(orig->width,orig->height), IPL_DEPTH_8U, 1);
-    	grayUp = cvCreateImage(cvSize(orig->width,orig->height), IPL_DEPTH_8U, 1);
-    	prev_gray = cvCreateImage(cvSize(orig->width,orig->height), 8, 1);
-    	diff_8U = cvCreateImage(cvSize(orig->width,orig->height), 8, 1);
-    }
-    // Here we make a copy of the pixel data from image to orig->imageData
-    // orig is a IplImage struct, the default image type in openCV, take a look on the IplImage data structure here
-    // http://www.cs.iit.edu/~agam/cs512/lect-notes/opencv-intro/opencv-intro.html 
-    memcpy( gray->imageData, image.data, image.xsize*image.ysize );
-    
+    cv::Mat mat = image2mat(image);
+    // Convert to grayscale
+    cv::cvtColor(orig, gray, cv::COLOR_BGRA2GRAY);
 
     if (x_set) {
-    	memcpy( prev_gray->imageData, gray->imageData, image.xsize*image.ysize );
-	x_set=0;
+      prev_gray = gray.clone();
+      x_set=0;
     } 
 
-    cvSubS (prev_gray,cvScalar(x_threshold,x_threshold,x_threshold,x_threshold),grayLow,NULL);
-    cvAddS (prev_gray,cvScalar(x_threshold,x_threshold,x_threshold,x_threshold),grayUp,NULL);
-    cvInRange (gray, grayLow, grayUp, diff_8U);
+    cv::subtract(prev_gray,cv::Scalar(x_threshold), grayLow);
+    cv::add(prev_gray,cv::Scalar(x_threshold), grayUp);
+    cv::inRange (gray, grayLow, grayUp, diff_8U);
 
-    cvNot (diff_8U,diff_8U);
+    cv::bitwise_not(diff_8U,diff_8U);
 
-    //copy back the processed frame to image
-    memcpy( image.data, diff_8U->imageData, image.xsize*image.ysize );
+    mat2image(diff_8U, image);
 }
 
 /////////////////////////////////////////////////////////
--- src/pix_opencv_bgsubstract.h
+++ src/pix_opencv_bgsubstract.h
@@ -1,27 +1,6 @@
-/*-----------------------------------------------------------------
-LOG
-    GEM - Graphics Environment for Multimedia
-
-    Change pix to greyscale
-
-    Copyright (c) 1997-1999 Mark Danks. mark@danks.org
-    Copyright (c) Günther Geiger. geiger@epy.co.at
-    Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::für::umläute. IEM. zmoelnig@iem.kug.ac.at
-    Copyright (c) 2002 James Tittle & Chris Clepper
-    For information on usage and redistribution, and for a DISCLAIMER OF ALL
-    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
-
------------------------------------------------------------------*/
-
-#ifndef INCLUDE_PIX_OPENCV_BGSUBSTRACT_H_
-#define INCLUDE_PIX_OPENCV_BGSUBSTRACT_H_
-
-#ifndef _EiC
-#include "opencv2/core/core_c.h"
-#include "opencv2/imgproc/imgproc_c.h"
-#endif
-
-#include "Base/GemPixObj.h"
+#include <opencv2/core/core_c.h>
+#include <opencv2/imgproc/imgproc_c.h>
+#include <Base/GemPixObj.h>
 
 /*-----------------------------------------------------------------
 -------------------------------------------------------------------
@@ -34,7 +13,7 @@ KEYWORDS
     pix
     
 DESCRIPTION
-   
+
 -----------------------------------------------------------------*/
 class GEM_EXPORT pix_opencv_bgsubstract : public GemPixObj
 {
@@ -42,46 +21,45 @@ class GEM_EXPORT pix_opencv_bgsubstract : public GemPixObj
 
     public:
 
-	    //////////
-	    // Constructor
-    	pix_opencv_bgsubstract();
-    	
-    protected:
-    	
-    	//////////
-    	// Destructor
-    	virtual ~pix_opencv_bgsubstract();
+      //////////
+      // Constructor
+      pix_opencv_bgsubstract();
 
-    	//////////
-    	// Do the processing
-    	virtual void 	processRGBAImage(imageStruct &image);
-    	virtual void 	processRGBImage(imageStruct &image);
-	virtual void 	processYUVImage(imageStruct &image);
-    	virtual void 	processGrayImage(imageStruct &image); 
-    	
-	//////////
-    	// Set the new edge threshold
-    	void	    	floatThreshMess(float x_threshold);
-    	void	    	SetMess();
-    	// The new threshold
-	int 		x_threshold;
-	int 		x_set;
+  protected:
 
-	// to detect changes in the image size
-	int 		comp_xsize;
-	int		comp_ysize;
+    //////////
+    // Destructor
+    virtual ~pix_opencv_bgsubstract();
 
-    private:
+    //////////
+    // Do the processing
+    virtual void 	processImage(imageStruct &image);
+
+    //////////
+    // Set the new edge threshold
+    void	    	floatThreshMess(float x_threshold);
+    void	    	SetMess();
+    // The new threshold
+    int 		x_threshold;
+    int 		x_set;
+
+    // to detect changes in the image size
+    int 	comp_xsize;
+    int		comp_ysize;
+
+  private:
     
-    	//////////
-    	// Static member functions
-    	static void 	floatTreshMessCallback(void *data, t_floatarg thresh_value);
-    	static void 	SetMessCallback(void *data);
+    //////////
+    // Static member functions
+    static void 	floatTreshMessCallback(void *data, t_floatarg thresh_value);
+    static void 	SetMessCallback(void *data);
 
-	/////////
-	// IplImage needed
-    	IplImage 	*orig, *rgb, *gray, *prev_gray, *grayLow, *grayUp, *diff_8U;
-	
+    cv::Mat orig;
+    cv::Mat rgb;
+    cv::Mat gray;
+    cv::Mat prev_gray;
+    cv::Mat grayLow;
+    cv::Mat grayUp;
+    cv::Mat diff_8U;
+
 };
-
-#endif	// for header file
