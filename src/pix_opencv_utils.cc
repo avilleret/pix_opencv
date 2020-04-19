932b5253f4972d8afee0425c61f575213cca1091
Antoine Villeret <antoine.villeret@gmail.com>
2020-04-19 16:45:01 +0200

    add image2hsv and support bgr image

--- src/pix_opencv_utils.cc
+++ src/pix_opencv_utils.cc
@@ -11,8 +11,13 @@ cv::Mat image2mat(imageStruct& image)
   {
     case 1:
       mat = cv::Mat(image.ysize, image.xsize, CV_8UC1, image.data);
+      break;
+    case 3:
+      mat = cv::Mat(image.ysize, image.xsize, CV_8UC3, image.data);
+      break;
     case 4:
       mat = cv::Mat(image.ysize, image.xsize, CV_8UC4, image.data);
+      break;
     default:
       error("image format not supported");
   }
@@ -26,13 +31,41 @@ cv::Mat image2mat_gray(imageStruct& image)
   {
     case 1:
       gray = cv::Mat(image.ysize, image.xsize, CV_8UC1, image.data);
+      break;
+    case 3:
+      mat = cv::Mat(image.ysize, image.xsize, CV_8UC3, image.data);
+      cv::cvtColor(mat, gray, cv::COLOR_RGB2GRAY);
+      break;
     case 4:
       mat = cv::Mat(image.ysize, image.xsize, CV_8UC4, image.data);
       cv::cvtColor(mat, gray, cv::COLOR_RGBA2GRAY);
+      break;
     default:
       error("image format not supported");
   }
   return gray;
+}
+
+cv::Mat image2mat_hsv(imageStruct& image)
+{
+  cv::Mat mat, hsv, bgr;
+  switch(image.csize)
+  {
+    case 1:
+      mat = cv::Mat(image.ysize, image.xsize, CV_8UC3, image.data);
+      cv::cvtColor(mat, bgr, cv::COLOR_GRAY2BGR);
+      cv::cvtColor(bgr, hsv, cv::COLOR_BGR2HSV);
+    case 3:
+      mat = cv::Mat(image.ysize, image.xsize, CV_8UC3, image.data);
+      cv::cvtColor(mat, bgr, cv::COLOR_BGR2HSV);
+    case 4:
+      mat = cv::Mat(image.ysize, image.xsize, CV_8UC4, image.data);
+      cv::cvtColor(mat, bgr, cv::COLOR_BGRA2BGR);
+      cv::cvtColor(bgr, hsv, cv::COLOR_BGR2HSV);
+    default:
+      error("image format not supported");
+  }
+  return hsv;
 }
 
 void mat2image(const cv::Mat& mat, imageStruct& image)
@@ -50,6 +83,12 @@ void mat2image(const cv::Mat& mat, imageStruct& image)
       case 1:
         image.data = mat.data;
         break;
+      case 3:
+      {
+        cv::Mat rgb = image2mat(image);
+        cv::cvtColor(mat, rgb, cv::COLOR_GRAY2RGB);
+        break;
+      }
       case 4:
       {
         cv::Mat rgba = image2mat(image);
--- src/pix_opencv_utils.h
+++ src/pix_opencv_utils.h
@@ -5,4 +5,5 @@
 
 cv::Mat image2mat(imageStruct& image);
 cv::Mat image2mat_gray(imageStruct& image);
+cv::Mat image2mat_hsv(imageStruct& image);
 void mat2image(const cv::Mat& mat, imageStruct& image);
