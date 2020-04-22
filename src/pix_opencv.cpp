782d1933f5ac2bf0545fa0d6cff1ac1b40a827ec
Antoine Villeret <antoine.villeret@gmail.com>
2020-04-22 11:41:26 +0200

    setup dft and distrans on library loading

--- src/pix_opencv.cpp
+++ src/pix_opencv.cpp
@@ -10,6 +10,8 @@ extern "C" void pix_opencv_camshift_setup();
 extern "C" void pix_opencv_clahe_setup();
 extern "C" void pix_opencv_colorfilt_setup();
 extern "C" void pix_opencv_contours_setup();
+extern "C" void pix_opencv_dft_setup();
+extern "C" void pix_opencv_distrans_setup();
 
 extern "C" {
 
@@ -55,6 +57,8 @@ void pix_opencv_setup(void)
     pix_opencv_clahe_setup();
     pix_opencv_colorfilt_setup();
     pix_opencv_contours_setup();
+    pix_opencv_dft_setup();
+    pix_opencv_distrans_setup();
 }
 
 } // extern "C"
