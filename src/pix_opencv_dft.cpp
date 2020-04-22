4b2d6597ffb42c8ca4fb086b16b0601b6b89c394
Antoine Villeret <antoine.villeret@gmail.com>
2020-04-22 09:34:50 +0200

    rewrite dft with C++ API

--- src/pix_opencv_dft.cpp
+++ src/pix_opencv_dft.cpp
@@ -1,21 +1,5 @@
-////////////////////////////////////////////////////////
-//
-// GEM - Graphics Environment for Multimedia
-//
-// zmoelnig@iem.kug.ac.at
-//
-// Implementation file
-//
-//    Copyright (c) 1997-2000 Mark Danks.
-//    Copyright (c) Günther Geiger.
-//    Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::für::umläute. IEM
-//    Copyright (c) 2002 James Tittle & Chris Clepper
-//    For information on usage and redistribution, and for a DISCLAIMER OF ALL
-//    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
-//
-/////////////////////////////////////////////////////////
-
-#include "pix_opencv_dft.h"
+#include "pix_opencv_dft.hpp"
+#include "pix_opencv_utils.hpp"
 
 CPPEXTERN_NEW(pix_opencv_dft)
 
@@ -29,23 +13,7 @@ CPPEXTERN_NEW(pix_opencv_dft)
 /////////////////////////////////////////////////////////
 
 pix_opencv_dft :: pix_opencv_dft()
-{ 
-  int i;
-
-  x_calculate = 1;
-  comp_xsize=0;
-  comp_ysize=0;
-
-  rgb = NULL;
-  rgba = NULL;
-  gray = NULL;
-  input_re = NULL;
-  input_im = NULL;
-  input_co = NULL;
-  dft_A = NULL;
-  image_re = NULL;
-  image_im = NULL;
-  image_mout = NULL;
+{
 }
 
 /////////////////////////////////////////////////////////
@@ -54,395 +22,54 @@ pix_opencv_dft :: pix_opencv_dft()
 /////////////////////////////////////////////////////////
 pix_opencv_dft :: ~pix_opencv_dft()
 {
-    //Destroy cv_images to clean memory
-    cvReleaseImage( &rgb );
-    cvReleaseImage( &rgba );
-    //cvReleaseImage( &gray );
-    cvReleaseImage( &input_re );
-    cvReleaseImage( &input_im );
-    cvReleaseImage( &input_co );
-    cvReleaseMat( &dft_A );
-    //cvReleaseImage( &mage_re );
-    //cvReleaseImage( &image_im );
-    //cvReleaseImage( &image_mout );
-
-}
-
-/////////////////////////////////////////////////////////
-// shiftDFT
-//
-/////////////////////////////////////////////////////////
-void pix_opencv_dft :: shiftDFT(CvArr * src_arr, CvArr * dst_arr )
-{
-    CvMat *tmp=NULL;
-    CvMat q1stub, q2stub;
-    CvMat q3stub, q4stub;
-    CvMat d1stub, d2stub;
-    CvMat d3stub, d4stub;
-    CvMat * q1, * q2, * q3, * q4;
-    CvMat * d1, * d2, * d3, * d4;
-
-    CvSize size = cvGetSize(src_arr);
-    CvSize dst_size = cvGetSize(dst_arr);
-    int cx, cy;
-
-    if(dst_size.width != size.width ||
-       dst_size.height != size.height){
-            return;
-    }
-
-    if(src_arr==dst_arr){
-        tmp = cvCreateMat(size.height/2, size.width/2, cvGetElemType(src_arr));
-    }
-
-    cx = size.width/2;
-    cy = size.height/2; // image center
-
-    q1 = cvGetSubRect( src_arr, &q1stub, cvRect(0,0,cx, cy) );
-    q2 = cvGetSubRect( src_arr, &q2stub, cvRect(cx,0,cx,cy) );
-    q3 = cvGetSubRect( src_arr, &q3stub, cvRect(cx,cy,cx,cy) );
-    q4 = cvGetSubRect( src_arr, &q4stub, cvRect(0,cy,cx,cy) );
-    d1 = cvGetSubRect( src_arr, &d1stub, cvRect(0,0,cx,cy) );
-    d2 = cvGetSubRect( src_arr, &d2stub, cvRect(cx,0,cx,cy) );
-    d3 = cvGetSubRect( src_arr, &d3stub, cvRect(cx,cy,cx,cy) );
-    d4 = cvGetSubRect( src_arr, &d4stub, cvRect(0,cy,cx,cy) );
-
-    if(src_arr!=dst_arr)
-    {
-        if( !CV_ARE_TYPES_EQ( q1, d1 )){
-            return;
-        }
-        cvCopy(q3, d1, 0);
-        cvCopy(q4, d2, 0);
-        cvCopy(q1, d3, 0);
-        cvCopy(q2, d4, 0);
-    }
-    else
-    {
-        cvCopy(q3, tmp, 0);
-        cvCopy(q1, q3, 0);
-        cvCopy(tmp, q1, 0);
-        cvCopy(q4, tmp, 0);
-        cvCopy(q2, q4, 0);
-        cvCopy(tmp, q2, 0);
-    }
-    if(src_arr==dst_arr){
-        cvReleaseMat( &tmp );
-    }
-
 }
 
 /////////////////////////////////////////////////////////
 // processImage
 //
 /////////////////////////////////////////////////////////
-void pix_opencv_dft :: processRGBAImage(imageStruct &image)
+void pix_opencv_dft :: processImage(imageStruct &image)
 {
-  unsigned char *pixels = image.data;
-  int i;
-  CvMat tmp;
-  double m,M;
-  int px,py;
+  if ( x_calculate )
+  {
+    cv::Mat I = image2mat_gray(image);
+    cv::Mat padded;                            //expand input image to optimal size
+    int m = cv::getOptimalDFTSize( I.rows );
+    int n = cv::getOptimalDFTSize( I.cols ); // on the border add zero values
+    copyMakeBorder(I, padded, 0, m - I.rows, 0, n - I.cols, cv::BORDER_CONSTANT, cv::Scalar::all(0));
+    cv::Mat planes[] = {cv::Mat_<float>(padded), cv::Mat::zeros(padded.size(), CV_32F)};
+    cv::Mat complexI;
+    merge(planes, 2, complexI);         // Add to the expanded another plane with zeros
+    dft(complexI, complexI);            // this way the result may fit in the source matrix
+    // compute the magnitude and switch to logarithmic scale
+    // => log(1 + sqrt(Re(DFT(I))^2 + Im(DFT(I))^2))
+    split(complexI, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
+    magnitude(planes[0], planes[1], planes[0]);// planes[0] = magnitude
+    cv::Mat magI = planes[0];
+    magI += cv::Scalar::all(1);                    // switch to logarithmic scale
+    log(magI, magI);
+    // crop the spectrum, if it has an odd number of rows or columns
+    magI = magI(cv::Rect(0, 0, magI.cols & -2, magI.rows & -2));
+    // rearrange the quadrants of Fourier image  so that the origin is at the image center
+    int cx = magI.cols/2;
+    int cy = magI.rows/2;
+    cv::Mat q0(magI, cv::Rect(0, 0, cx, cy));   // Top-Left - Create a ROI per quadrant
+    cv::Mat q1(magI, cv::Rect(cx, 0, cx, cy));  // Top-Right
+    cv::Mat q2(magI, cv::Rect(0, cy, cx, cy));  // Bottom-Left
+    cv::Mat q3(magI, cv::Rect(cx, cy, cx, cy)); // Bottom-Right
+    cv::Mat tmp;                           // swap quadrants (Top-Left with Bottom-Right)
+    q0.copyTo(tmp);
+    q3.copyTo(q0);
+    tmp.copyTo(q3);
+    q1.copyTo(tmp);                    // swap quadrant (Top-Right with Bottom-Left)
+    q2.copyTo(q1);
+    tmp.copyTo(q2);
+    cv::normalize(magI, magI, 0, 1, cv::NORM_MINMAX); // Transform the matrix with float values into a
+                                            // viewable image form (float between values 0 and 1).
 
-  if ((this->comp_xsize!=image.xsize)&&(this->comp_ysize!=image.ysize)) {
-
-        this->comp_xsize=image.xsize;
-        this->comp_ysize=image.ysize;
-
-    	//Destroy cv_images to clean memory
-        cvReleaseImage( &rgb );
-        cvReleaseImage( &rgba );
-        //cvReleaseImage( &gray );
-        cvReleaseImage( &input_re );
-        cvReleaseImage( &input_im );
-        cvReleaseImage( &input_co );
-        cvReleaseMat( &dft_A );
-        //cvReleaseImage( &image_re );
-        //cvReleaseImage( &image_im );
-        //cvReleaseImage( &image_mout );
-        //cvReleaseImage( &image_pout );
-
-	//Create cv_images 
-        rgb = cvCreateImage(cvSize(image.xsize,image.ysize), IPL_DEPTH_8U, 3);
-        rgba = cvCreateImage(cvSize(image.xsize,image.ysize), IPL_DEPTH_8U, 4);
-        gray = cvCreateImage(cvSize(image.xsize,image.ysize), IPL_DEPTH_8U, 1);
-        input_re = cvCreateImage( cvGetSize(rgb), IPL_DEPTH_64F, 1);
-        input_im = cvCreateImage( cvGetSize(rgb), IPL_DEPTH_64F, 1);
-        input_co = cvCreateImage( cvGetSize(rgb), IPL_DEPTH_64F, 2);
-        dft_M = cvGetOptimalDFTSize( image.ysize - 1 );
-        dft_N = cvGetOptimalDFTSize( image.xsize - 1 );
-        dft_A = cvCreateMat( dft_M, dft_N, CV_64FC2 );
-        image_re = cvCreateImage( cvSize(dft_N, dft_M), IPL_DEPTH_64F, 1);
-        image_im = cvCreateImage( cvSize(dft_N, dft_M), IPL_DEPTH_64F, 1);
-        image_mout = cvCreateImage( cvSize(dft_N, dft_M), IPL_DEPTH_8U, 1);
-
-    }
-
-    memcpy( rgba->imageData, image.data, image.xsize*image.ysize*4 );
-    cvCvtColor(rgba, gray, CV_BGRA2GRAY);
-
-    if ( x_calculate )
-    {
-      // discrete fourier transform
-      cvScale(gray, input_re, 1.0, 0.0);
-      cvZero(input_im);
-      cvMerge(input_re, input_im, NULL, NULL, input_co);
- 
-      // copy A to dft_A and pad dft_A with zeros
-      cvGetSubRect( dft_A, &tmp, cvRect(0,0, gray->width, gray->height));
-      cvCopy( input_co, &tmp, NULL );
-      if( dft_A->cols > gray->width )
-      {
-          cvGetSubRect( dft_A, &tmp, cvRect(gray->width,0, dft_A->cols - gray->width, gray->height));
-          cvZero( &tmp );
-      }
- 
-      // no need to pad bottom part of dft_A with zeros because of
-      // use nonzero_rows parameter in cvDFT() call below
-      cvDFT( dft_A, dft_A, CV_DXT_FORWARD, input_co->height );
- 
-      // Split Fourier in real and imaginary parts
-      cvSplit( dft_A, image_re, image_im, 0, 0 );
- 
-      // Compute the magnitude of the spectrum Mag = sqrt(Re^2 + Im^2)
-      cvPow( image_re, image_re, 2.0);
-      cvPow( image_im, image_im, 2.0);
-      cvAdd( image_re, image_im, image_re, NULL);
-      cvPow( image_re, image_re, 0.5 );
-
-      // Compute log(1 + Mag)
-      cvAddS( image_re, cvScalarAll(1.0), image_re, NULL ); // 1 + Mag
-      cvLog( image_re, image_re ); // log(1 + Mag)
-
-      // Rearrange the quadrants of Fourier image so that the origin is at
-      // the image center
-      this->shiftDFT( image_re, image_re );
-
-      // normalize image
-      cvMinMaxLoc(image_re, &m, &M, NULL, NULL, NULL);
-      cvScale(image_re, image_re, 255.0/(M-m), 255.0*(-m)/(M-m));
-
-      for( py=0; py<image_re->height; py++ ) {
-         double* ptri = (double*) ( image_re->imageData + py * image_re->widthStep);
-         unsigned char* ptrp = (unsigned char*) ( image_mout->imageData + py * image_mout->widthStep);
-         for( px=0; px<image_re->width; px++ ) {
-           if ( *(ptrp+px) > 255.0 ) post( "pix_opencv_dft : error value over 255" );
-           (*(ptrp+px)) = (unsigned char)( (*(ptri+px)) );
-         }
-      }
-
-      x_calculate=0;
-    }
-
-    cvCvtColor(image_mout, rgba, CV_GRAY2RGBA);
-    memcpy( image.data, rgba->imageData, image.xsize*image.ysize*4 );
-}
-
-void pix_opencv_dft :: processRGBImage(imageStruct &image)
-{
-  unsigned char *pixels = image.data;
-  int i;
-  CvMat tmp;
-  double m,M;
-  int px,py;
-
-    if ((this->comp_xsize!=image.xsize)&&(this->comp_ysize!=image.ysize)) {
-
-        this->comp_xsize=image.xsize;
-        this->comp_ysize=image.ysize;
-
-    	//Destroy cv_images to clean memory
-        cvReleaseImage( &rgb );
-        cvReleaseImage( &rgba );
-        //cvReleaseImage( &gray );
-        cvReleaseImage( &input_re );
-        cvReleaseImage( &input_im );
-        cvReleaseImage( &input_co );
-        cvReleaseMat( &dft_A );
-        //cvReleaseImage( &image_re );
-        //cvReleaseImage( &image_im );
-        //cvReleaseImage( &image_mout );
-        //cvReleaseImage( &image_pout );
-
-	//Create cv_images 
-        rgb = cvCreateImage(cvSize(image.xsize,image.ysize), IPL_DEPTH_8U, 3);
-        rgba = cvCreateImage(cvSize(image.xsize,image.ysize), IPL_DEPTH_8U, 4);
-        gray = cvCreateImage(cvSize(image.xsize,image.ysize), IPL_DEPTH_8U, 1);
-        input_re = cvCreateImage( cvGetSize(rgb), IPL_DEPTH_64F, 1);
-        input_im = cvCreateImage( cvGetSize(rgb), IPL_DEPTH_64F, 1);
-        input_co = cvCreateImage( cvGetSize(rgb), IPL_DEPTH_64F, 2);
-        dft_M = cvGetOptimalDFTSize( image.ysize - 1 );
-        dft_N = cvGetOptimalDFTSize( image.xsize - 1 );
-        dft_A = cvCreateMat( dft_M, dft_N, CV_64FC2 );
-        image_re = cvCreateImage( cvSize(dft_N, dft_M), IPL_DEPTH_64F, 1);
-        image_im = cvCreateImage( cvSize(dft_N, dft_M), IPL_DEPTH_64F, 1);
-        image_mout = cvCreateImage( cvSize(dft_N, dft_M), IPL_DEPTH_8U, 1);
-
-    }
-
-    memcpy( rgb->imageData, image.data, image.xsize*image.ysize*3 );
-    cvCvtColor(rgb, gray, CV_BGR2GRAY);
-
-    if ( x_calculate )
-    {
-      // discrete fourier transform
-      cvScale(gray, input_re, 1.0, 0.0);
-      cvZero(input_im);
-      cvMerge(input_re, input_im, NULL, NULL, input_co);
- 
-      // copy A to dft_A and pad dft_A with zeros
-      cvGetSubRect( dft_A, &tmp, cvRect(0,0, gray->width, gray->height));
-      cvCopy( input_co, &tmp, NULL );
-      if( dft_A->cols > gray->width )
-      {
-          cvGetSubRect( dft_A, &tmp, cvRect(gray->width,0, dft_A->cols - gray->width, gray->height));
-          cvZero( &tmp );
-      }
- 
-      // no need to pad bottom part of dft_A with zeros because of
-      // use nonzero_rows parameter in cvDFT() call below
-      cvDFT( dft_A, dft_A, CV_DXT_FORWARD, input_co->height );
- 
-      // Split Fourier in real and imaginary parts
-      cvSplit( dft_A, image_re, image_im, 0, 0 );
- 
-      // Compute the magnitude of the spectrum Mag = sqrt(Re^2 + Im^2)
-      cvPow( image_re, image_re, 2.0);
-      cvPow( image_im, image_im, 2.0);
-      cvAdd( image_re, image_im, image_re, NULL);
-      cvPow( image_re, image_re, 0.5 );
-
-      // Compute log(1 + Mag)
-      cvAddS( image_re, cvScalarAll(1.0), image_re, NULL ); // 1 + Mag
-      cvLog( image_re, image_re ); // log(1 + Mag)
-
-      // Rearrange the quadrants of Fourier image so that the origin is at
-      // the image center
-      this->shiftDFT( image_re, image_re );
-
-      // normalize image
-      cvMinMaxLoc(image_re, &m, &M, NULL, NULL, NULL);
-      cvScale(image_re, image_re, 255.0/(M-m), 255.0*(-m)/(M-m));
-
-      for( py=0; py<image_re->height; py++ ) {
-         double* ptri = (double*) ( image_re->imageData + py * image_re->widthStep);
-         unsigned char* ptrp = (unsigned char*) ( image_mout->imageData + py * image_mout->widthStep);
-         for( px=0; px<image_re->width; px++ ) {
-           if ( *(ptrp+px) > 255.0 ) post( "pix_opencv_dft : error value over 255" );
-           (*(ptrp+px)) = (unsigned char)( (*(ptri+px)) );
-         }
-      }
-
-      x_calculate=0;
-    }
-
-    cvCvtColor(image_mout, rgb, CV_GRAY2RGB);
-    memcpy( image.data, rgb->imageData, image.xsize*image.ysize*3 );
-}
-
-void pix_opencv_dft :: processYUVImage(imageStruct &image)
-{
-  post( "pix_opencv_contours_convexity : yuv format not supported" );
-}
-    	
-void pix_opencv_dft :: processGrayImage(imageStruct &image)
-{ 
-  unsigned char *pixels = image.data;
-  int i;
-  CvMat tmp;
-  double m,M;
-  int px,py;
-
-    if ((this->comp_xsize!=image.xsize)&&(this->comp_ysize!=image.ysize)) {
-
-        this->comp_xsize=image.xsize;
-        this->comp_ysize=image.ysize;
-
-    	//Destroy cv_images to clean memory
-        cvReleaseImage( &rgb );
-        cvReleaseImage( &rgba );
-        //cvReleaseImage( &gray );
-        cvReleaseImage( &input_re );
-        cvReleaseImage( &input_im );
-        cvReleaseImage( &input_co );
-        cvReleaseMat( &dft_A );
-        //cvReleaseImage( &image_re );
-        //cvReleaseImage( &image_im );
-        //cvReleaseImage( &image_mout );
-        //cvReleaseImage( &image_pout );
-
-	//Create cv_images 
-        rgb = cvCreateImage(cvSize(image.xsize,image.ysize), IPL_DEPTH_8U, 3);
-        rgba = cvCreateImage(cvSize(image.xsize,image.ysize), IPL_DEPTH_8U, 4);
-        gray = cvCreateImage(cvSize(image.xsize,image.ysize), IPL_DEPTH_8U, 1);
-        input_re = cvCreateImage( cvGetSize(rgb), IPL_DEPTH_64F, 1);
-        input_im = cvCreateImage( cvGetSize(rgb), IPL_DEPTH_64F, 1);
-        input_co = cvCreateImage( cvGetSize(rgb), IPL_DEPTH_64F, 2);
-        dft_M = cvGetOptimalDFTSize( image.ysize - 1 );
-        dft_N = cvGetOptimalDFTSize( image.xsize - 1 );
-        dft_A = cvCreateMat( dft_M, dft_N, CV_64FC2 );
-        image_re = cvCreateImage( cvSize(dft_N, dft_M), IPL_DEPTH_64F, 1);
-        image_im = cvCreateImage( cvSize(dft_N, dft_M), IPL_DEPTH_64F, 1);
-        image_mout = cvCreateImage( cvSize(dft_N, dft_M), IPL_DEPTH_8U, 1);
-
-    }
-
-    memcpy( gray->imageData, image.data, image.xsize*image.ysize );
-
-    if ( x_calculate )
-    {
-      // discrete fourier transform
-      cvScale(gray, input_re, 1.0, 0.0);
-      cvZero(input_im);
-      cvMerge(input_re, input_im, NULL, NULL, input_co);
- 
-      // copy A to dft_A and pad dft_A with zeros
-      cvGetSubRect( dft_A, &tmp, cvRect(0,0, gray->width, gray->height));
-      cvCopy( input_co, &tmp, NULL );
-      if( dft_A->cols > gray->width )
-      {
-          cvGetSubRect( dft_A, &tmp, cvRect(gray->width,0, dft_A->cols - gray->width, gray->height));
-          cvZero( &tmp );
-      }
- 
-      // no need to pad bottom part of dft_A with zeros because of
-      // use nonzero_rows parameter in cvDFT() call below
-      cvDFT( dft_A, dft_A, CV_DXT_FORWARD, input_co->height );
- 
-      // Split Fourier in real and imaginary parts
-      cvSplit( dft_A, image_re, image_im, 0, 0 );
- 
-      // Compute the magnitude of the spectrum Mag = sqrt(Re^2 + Im^2)
-      cvPow( image_re, image_re, 2.0);
-      cvPow( image_im, image_im, 2.0);
-      cvAdd( image_re, image_im, image_re, NULL);
-      cvPow( image_re, image_re, 0.5 );
-
-      // Compute log(1 + Mag)
-      cvAddS( image_re, cvScalarAll(1.0), image_re, NULL ); // 1 + Mag
-      cvLog( image_re, image_re ); // log(1 + Mag)
-
-      // Rearrange the quadrants of Fourier image so that the origin is at
-      // the image center
-      this->shiftDFT( image_re, image_re );
-
-      // normalize image
-      cvMinMaxLoc(image_re, &m, &M, NULL, NULL, NULL);
-      cvScale(image_re, image_re, 255.0/(M-m), 255.0*(-m)/(M-m));
-
-      for( py=0; py<image_re->height; py++ ) {
-         double* ptri = (double*) ( image_re->imageData + py * image_re->widthStep);
-         unsigned char* ptrp = (unsigned char*) ( image_mout->imageData + py * image_mout->widthStep);
-         for( px=0; px<image_re->width; px++ ) {
-           if ( *(ptrp+px) > 255.0 ) post( "pix_opencv_dft : error value over 255" );
-           (*(ptrp+px)) = (unsigned char)( (*(ptri+px)) );
-         }
-      }
-
-      x_calculate=0;
-    }
-
-    memcpy( image.data, image_mout->imageData, image.xsize*image.ysize );
+    mat2image(magI,image);
+    x_calculate=0;
+  }
 }
 
 /////////////////////////////////////////////////////////
--- src/pix_opencv_dft.hpp
+++ src/pix_opencv_dft.hpp
@@ -1,27 +1,7 @@
-/*-----------------------------------------------------------------
-LOG
-    GEM - Graphics Environment for Multimedia
-
-    Discrete Fourier Transform
-
-    Copyright (c) 1997-1999 Mark Danks. mark@danks.org
-    Copyright (c) Günther Geiger. geiger@epy.co.at
-    Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::für::umläute. IEM. zmoelnig@iem.kug.ac.at
-    Copyright (c) 2002 James Tittle & Chris Clepper
-    For information on usage and redistribution, and for a DISCLAIMER OF ALL
-    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
-
------------------------------------------------------------------*/
-
-#ifndef INCLUDE_PIX_OPENCV_DFT_H_
-#define INCLUDE_PIX_OPENCV_DFT_H_
-
-#ifndef _EiC
-#include "opencv2/core/core_c.h"
-#include "opencv2/imgproc/imgproc_c.h"
-#endif
-
-#include "Base/GemPixObj.h"
+#pragma once
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <Base/GemPixObj.h>
 
 /*-----------------------------------------------------------------
 -------------------------------------------------------------------
@@ -34,56 +14,33 @@ KEYWORDS
     pix
     
 DESCRIPTION
-   
+
 -----------------------------------------------------------------*/
 class GEM_EXPORT pix_opencv_dft : public GemPixObj
 {
-    CPPEXTERN_HEADER(pix_opencv_dft, GemPixObj)
+  CPPEXTERN_HEADER(pix_opencv_dft, GemPixObj)
 
-    public:
+  public:
 
-	//////////
-	// Constructor
-    	pix_opencv_dft();
-    	
-    protected:
-    	
-    	//////////
-    	// Destructor
-    	virtual ~pix_opencv_dft();
+    //////////
+    // Constructor
+    pix_opencv_dft();
 
-    	//////////
-    	// Do the processing
-    	virtual void 	processRGBAImage(imageStruct &image);
-    	virtual void 	processRGBImage(imageStruct &image);
-	virtual void 	processYUVImage(imageStruct &image);
-    	virtual void 	processGrayImage(imageStruct &image); 
+  protected:
 
-        int comp_xsize;
-        int comp_ysize;
-        int x_calculate;
-        int dft_M;
-        int dft_N;
+    //////////
+    // Destructor
+    ~pix_opencv_dft();
 
-    private:
+    //////////
+    // Do the processing
+    void 	processImage(imageStruct &image) override;
+
+    int x_calculate{1};
+
+  private:
     
-    	//////////
-    	// Static member functions
-    	static void 	calculateCallback(void *data);
-        static void     shiftDFT(CvArr*, CvArr*);
-
-	// The output and temporary images
-        IplImage  *rgb;
-        IplImage  *rgba;
-        IplImage  *gray;
-        IplImage  *input_re;
-        IplImage  *input_im;
-        IplImage  *input_co;
-        CvMat     *dft_A;
-        IplImage  *image_re;
-        IplImage  *image_im;
-        IplImage  *image_mout;
-	
+    //////////
+    // Static member functions
+    static void 	calculateCallback(void *data);
 };
-
-#endif	// for header file
