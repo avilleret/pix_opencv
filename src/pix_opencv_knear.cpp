7977f068a4357bd9bcfa0d0c24dce9e88a18cc7b
Antoine Villeret <antoine.villeret@gmail.com>
2020-05-23 00:12:32 +0200

    wip on knear

--- src/pix_opencv_knear.cpp
+++ src/pix_opencv_knear.cpp
@@ -1,22 +1,7 @@
-////////////////////////////////////////////////////////
-//
-// GEM - Graphics Environment for Multimedia
-//
-// zmoelnig@iem.kug.ac.at
-//
-// Implementation file
-//
-//    Copyright (c) 1997-2000 Mark Danks.
-//    Copyright (c) Günther Geiger.
-//    Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::für::umläute. IEM
-//    Copyright (c) 2002 James Tittle & Chris Clepper
-//    For information on usage and redistribution, and for a DISCLAIMER OF ALL
-//    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
-//
-/////////////////////////////////////////////////////////
-
-#include "pix_opencv_knear.h"
-
+#include "pix_opencv_knear.hpp"
+#include "pix_opencv_utils.hpp"
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 CPPEXTERN_NEW_WITH_TWO_ARGS(pix_opencv_knear, t_symbol *, A_DEFSYM, t_floatarg, A_DEFFLOAT )
 
@@ -34,26 +19,25 @@ CPPEXTERN_NEW_WITH_TWO_ARGS(pix_opencv_knear, t_symbol *, A_DEFSYM, t_floatarg, 
 /////////////////////////////////////////////////////////
 
 
-void pix_opencv_knear :: findX(IplImage* imgSrc,int* min, int* max)
+void pix_opencv_knear :: findX(const cv::Mat& imgSrc,int& min, int& max)
 {
   int i;
   int minFound=0;
-  CvMat data;
-  CvScalar maxVal=cvRealScalar(imgSrc->width * 255);
-  CvScalar val=cvRealScalar(0);
+  cv::Scalar maxVal(imgSrc.cols * 255);
+  cv::Scalar val(0);
 
   // for each col sum, if sum < width*255 then we find the min
   // then continue to end to search the max, if sum< width*255 then is new max
-  for (i=0; i< imgSrc->width; i++)
+  for (i=0; i< imgSrc.cols; i++)
   {
-    cvGetCol(imgSrc, &data, i);
-    val= cvSum(&data);
+    auto col = imgSrc.col(i);
+    val= cv::sum(col);
     if(val.val[0] < maxVal.val[0])
     {
-      *max= i;
+      max= i;
       if(!minFound)
       {
-        *min= i;
+         min= i;
          minFound= 1;
       }
     }
@@ -61,27 +45,26 @@ void pix_opencv_knear :: findX(IplImage* imgSrc,int* min, int* max)
   }
 }
 
-void pix_opencv_knear :: findY(IplImage* imgSrc,int* min, int* max)
+void pix_opencv_knear :: findY(const cv::Mat& imgSrc,int& min, int& max)
 {
   int i;
   int minFound=0;
-  CvMat data;
-  CvScalar maxVal=cvRealScalar(imgSrc->width * 255);
-  CvScalar val=cvRealScalar(0);
+  cv::Scalar maxVal(imgSrc.rows * 255);
+  cv::Scalar val(0);
 
-  // for each col sum, if sum < width*255 then we find the min
+  // for each row sum, if sum < height*255 then we find the min
   // then continue to end to search the max, if sum< width*255 then is new max
-  for (i=0; i< imgSrc->height; i++)
+  for (i=0; i< imgSrc.rows; i++)
   {
-    cvGetRow(imgSrc, &data, i);
-    val= cvSum(&data);
+    auto row = imgSrc.row(i);
+    val= cv::sum(row);
     if(val.val[0] < maxVal.val[0])
     {
-      *max=i;
+      max=i;
       if(!minFound)
       {
-        *min= i;
-         minFound= 1;
+        min= i;
+        minFound= 1;
       }
     }
   }
@@ -94,73 +77,61 @@ void pix_opencv_knear :: findY(IplImage* imgSrc,int* min, int* max)
 //
 /////////////////////////////////////////////////////////
 
-CvRect pix_opencv_knear :: findBB(IplImage* imgSrc)
+cv::Rect pix_opencv_knear :: findBB(const cv::Mat& imgSrc)
 {
-  CvRect aux;
   int xmin, xmax, ymin, ymax;
   xmin=xmax=ymin=ymax=0;
 
-  this->findX(imgSrc, &xmin, &xmax);
-  this->findY(imgSrc, &ymin, &ymax);
+  this->findX(imgSrc, xmin, xmax);
+  this->findY(imgSrc, ymin, ymax);
 
-  aux=cvRect(xmin, ymin, xmax-xmin, ymax-ymin);
-
-  return aux;
-}
-
-IplImage pix_opencv_knear :: preprocessing(IplImage* imgSrc,int new_width, int new_height)
-{
-  IplImage* result;
-  IplImage* scaledResult;
-
-  CvMat data;
-  CvMat dataA;
-  CvRect bb;//bounding box
-
-  // find bounding box
-  bb=this->findBB(imgSrc);
+  cv::Rect bb = cv::Rect(xmin, ymin, xmax-xmin, ymax-ymin);
 
   if ( ( bb.width == 0 ) || ( bb.height == 0 ) )
   {
      bb.x = 0;
      bb.y = 0;
-     bb.width = imgSrc->width;
-     bb.height = imgSrc->height;
+     bb.width = imgSrc.cols;
+     bb.height = imgSrc.rows;
   }
 
+  return bb;
+}
+
+cv::Mat pix_opencv_knear :: preprocessing(const cv::Mat& imgSrc,int new_width, int new_height)
+{
+  cv::Rect bb = findBB(imgSrc);
+
   // get bounding box data and no with aspect ratio, the x and y can be corrupted
-  cvGetSubRect(imgSrc, &data, cvRect(bb.x, bb.y, bb.width, bb.height));
+  cv::Mat data = imgSrc(bb);
   // create image with this data with width and height with aspect ratio 1
   // then we get highest size betwen width and height of our bounding box
   int size=(bb.width>bb.height)?bb.width:bb.height;
-  result=cvCreateImage( cvSize( size, size ), 8, 1 );
-  cvSet(result,CV_RGB(255,255,255),NULL);
+  cv::Mat result = cv::Mat(size,size,CV_8UC1)*255;
   // copy de data in center of image
   int x=(int)floor((float)(size-bb.width)/2.0f);
   int y=(int)floor((float)(size-bb.height)/2.0f);
-  cvGetSubRect(result, &dataA, cvRect(x,y,bb.width, bb.height));
-  cvCopy(&data, &dataA, NULL);
+
+  cv::Mat dataA = result(cv::Rect(x,y,bb.width, bb.height));
+  data.copyTo(dataA);
   // scale result
-  scaledResult=cvCreateImage( cvSize( new_width, new_height ), 8, 1 );
-  cvResize(result, scaledResult, CV_INTER_NN);
+  cv::Mat scaledResult;
+  cv::resize(result, scaledResult, cv::Size(new_width, new_height), cv::INTER_NEAREST);
 
   // return processed data
-  return *scaledResult;
-
+  return scaledResult;
 }
 
 void pix_opencv_knear :: load_patterns(void)
 {
-  IplImage* src_image;
-  IplImage prs_image;
-  CvMat row,data;
+  cv::Mat prs_image;
+  cv::Mat data;
   char file[255];
-  int i=0,j;
-  CvMat row_header, *row1;
+  int i=0;
 
   this->x_rsamples = 0;
 
-  for( j = 0; j< this->x_nsamples; j++)
+  for(int j = 0; j< this->x_nsamples; j++)
   {
 
      // load fileath
@@ -171,22 +142,22 @@ void pix_opencv_knear :: load_patterns(void)
       std::string absolutePath = localPath + x_filepath;
       sprintf(file,"%s/%03d.png",absolutePath.c_str(), j);
     }
-     src_image = cvLoadImage(file,0);
-     if(!src_image)
+     cv::Mat src_image = cv::imread(file);
+     if(src_image.empty())
      {
        post("pix_opencv_knear : error: couldn't load image %s\n", file);
        continue;
      }
      if ( ( this->x_pwidth == -1 ) || ( this->x_pheight == -1 ) )
      {
-        this->x_pwidth = src_image->width;
-        this->x_pheight = src_image->height;
+        this->x_pwidth = src_image.cols;
+        this->x_pheight = src_image.rows;
         // post( "pix_opencv_knear : loaded : %s (%dx%d)", file, src_image->width, src_image->height);
         this->x_rsamples++;
      }
-     else if ( ( src_image->width != this->x_pwidth ) || ( src_image->height != this->x_pheight ) )
+     else if ( ( src_image.cols != this->x_pwidth ) || ( src_image.rows != this->x_pheight ) )
      {
-        post( "pix_opencv_knear : error : %s (%dx%d) : wrong size ( should be %dx%d )", file, src_image->width, src_image->height, this->x_pwidth, this->x_pheight);
+        post( "pix_opencv_knear : error : %s (%dx%d) : wrong size ( should be %dx%d )", file, src_image.cols, src_image.rows, this->x_pwidth, this->x_pheight);
         continue;
      }
      else
@@ -199,34 +170,32 @@ void pix_opencv_knear :: load_patterns(void)
      prs_image = this->preprocessing(src_image, this->x_pwidth, this->x_pheight);
      // post( "pix_opencv_knear : preprocessed : %s (%dx%d)", file, this->x_pwidth, this->x_pheight);
 
-     if ( ( this->trainData == NULL ) || ( this->trainClasses == NULL ))
-     {
-       this->trainData = cvCreateMat(this->x_nsamples, this->x_pwidth*this->x_pheight, CV_32FC1);
-       this->trainClasses = cvCreateMat(this->x_nsamples, 1, CV_32FC1);
-     }
+     trainData = cv::Mat(x_nsamples, x_pwidth*x_pheight, CV_32FC1);
+     trainClasses = cv::Mat(x_nsamples, 1, CV_32FC1);
 
      // set class label
-     cvGetRow(this->trainClasses, &row, j);
-     cvSet(&row, cvRealScalar(i), NULL);
-     // set data
-     cvGetRow(this->trainData, &row, j);
+     cv::Mat row = trainClasses.row(j);
+     row.at<float>(0) = i;
 
-     IplImage* img = cvCreateImage( cvSize( this->x_pwidth, this->x_pheight ), IPL_DEPTH_32F, 1 );
+     row = trainData.row(j);
+
+     cv::Mat img(cv::Size( this->x_pwidth, this->x_pheight ), CV_32FC1);
      // convert 8 bits image to 32 float image
-     cvConvertScale(&prs_image, img, 0.0039215, 0);
+     prs_image.convertTo(img, CV_32FC1, 0.0039215, 0);
 
-     cvGetSubRect(img, &data, cvRect( 0, 0, this->x_pwidth, this->x_pheight) );
+     data = img(cv::Rect( 0, 0, this->x_pwidth, this->x_pheight));
 
      // convert data matrix sizexsize to vecor
-     row1 = cvReshape( &data, &row_header, 0, 1 );
-     cvCopy(row1, &row, NULL);
-     cvReleaseImage( &img );
+     cv::Mat row1 = data.row(1);
+     row1.copyTo(row);
   }
 
   // create the classifier
   post( "pix_opencv_knear : loaded : %d samples from %s", this->x_rsamples, this->x_filepath);
   if ( this->x_rsamples == this->x_nsamples )
   {
+    x_knn = cv::ml::KNearest::create();
+    //x_knn->train(trainData, trainClasses, ...);
     this->knn=new CvKNearest( this->trainData, this->trainClasses, 0, false, this->x_nsamples );
     this->x_nearest=cvCreateMat(1,this->x_nsamples,CV_32FC1);
     this->x_dist=cvCreateMat(1,this->x_nsamples,CV_32FC1);
@@ -244,10 +213,6 @@ pix_opencv_knear :: pix_opencv_knear(t_symbol *path, t_floatarg nsamples)
 
   comp_xsize  = 320;
   comp_ysize = 240;
-
-  rgba = cvCreateImage( cvSize(comp_xsize, comp_ysize), 8, 4 );
-  rgb = cvCreateImage( cvSize(comp_xsize, comp_ysize), 8, 3 );
-  grey = cvCreateImage( cvSize(comp_xsize, comp_ysize), 8, 1 );
 
   if ( path && path != gensym("") && nsamples > 0. ){
     x_filepath = ( char * ) getbytes( 1024 );
@@ -279,13 +244,6 @@ pix_opencv_knear :: pix_opencv_knear(t_symbol *path, t_floatarg nsamples)
 /////////////////////////////////////////////////////////
 pix_opencv_knear :: ~pix_opencv_knear()
 {
-    // destroy cv structures
-    cvReleaseImage( &rgba );
-    cvReleaseImage( &rgb );
-    cvReleaseImage( &grey );
-    cvReleaseMat( &trainData );
-    cvReleaseMat( &trainClasses );
-
 }
 
 /////////////////////////////////////////////////////////
--- src/pix_opencv_knear.hpp
+++ src/pix_opencv_knear.hpp
@@ -1,28 +1,7 @@
-/*-----------------------------------------------------------------
-LOG
-    GEM - Graphics Environment for Multimedia
-
-    Simple distance classifier
-
-    Copyright (c) 1997-1999 Mark Danks. mark@danks.org
-    Copyright (c) Günther Geiger. geiger@epy.co.at
-    Copyright (c) 2001-2002 IOhannes m zmoelnig. forum::für::umläute. IEM. zmoelnig@iem.kug.ac.at
-    Copyright (c) 2002 James Tittle & Chris Clepper
-    For information on usage and redistribution, and for a DISCLAIMER OF ALL
-    WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
-
------------------------------------------------------------------*/
-
-#ifndef INCLUDE_PIX_OPENCV_KNEAR_H_
-#define INCLUDE_PIX_OPENCV_KNEAR_H_
-
-#ifndef _EiC
-#include "opencv2/legacy/legacy.hpp"
-#include "opencv2/highgui/highgui_c.h"
-#include "opencv2/ml/ml.hpp"
-#endif
-
-#include "Base/GemPixObj.h"
+#pragma once
+#include <opencv2/core.hpp>
+#include <opencv2/ml.hpp>
+#include <Base/GemPixObj.h>
 
 #include <string>
 #include <stdio.h>
@@ -30,16 +9,16 @@ LOG
 /*-----------------------------------------------------------------
 -------------------------------------------------------------------
 CLASS
-    
+
    pix_opencv_knear : OCR like pattern recognition
    based on basic OCR with Open CV tutorial
-   by damiles : http://blog.damiles.com/?p=93    
+   by damiles : http://blog.damiles.com/?p=93
 
 KEYWORDS
     pix
     
 DESCRIPTION
-   
+
 -----------------------------------------------------------------*/
 class GEM_EXPORT pix_opencv_knear : public GemPixObj
 {
@@ -47,62 +26,54 @@ class GEM_EXPORT pix_opencv_knear : public GemPixObj
 
     public:
 
-	    //////////
-	    // Constructor
-    	pix_opencv_knear(t_symbol *path, t_floatarg nsamples);
-    	
-    protected:
-    	
-    	//////////
-    	// Destructor
-    	virtual ~pix_opencv_knear();
+      //////////
+      // Constructor
+      pix_opencv_knear(t_symbol *path, t_floatarg nsamples);
 
-    	//////////
-    	// Do the processing
-    	virtual void 	processRGBAImage(imageStruct &image);
-    	virtual void 	processRGBImage(imageStruct &image);
-	virtual void 	processYUVImage(imageStruct &image);
-    	virtual void 	processGrayImage(imageStruct &image); 
-    	
-        void findX(IplImage* imgSrc,int* min, int* max);
-        void findY(IplImage* imgSrc,int* min, int* max);
-        CvRect findBB(IplImage* imgSrc);
-        IplImage preprocessing(IplImage* imgSrc,int new_width, int new_height);
-        void load_patterns(void);
+  protected:
 
-	// to detect changes in the image size
-	int 		comp_xsize;
-	int		comp_ysize;
+    //////////
+    // Destructor
+    virtual ~pix_opencv_knear();
+
+    //////////
+    // Do the processing
+    virtual void 	processImage(imageStruct &image);
+
+    void findX(const cv::Mat& imgSrc,int& min, int& max);
+    void findY(const cv::Mat& imgSrc,int& min, int& max);
+    cv::Rect findBB(const cv::Mat& imgSrc);
+    cv::Mat preprocessing(const cv::Mat& imgSrc,int new_width, int new_height);
+    void load_patterns(void);
+
+    // to detect changes in the image size
+    int 		comp_xsize;
+    int		comp_ysize;
     
 
-    private:
+  private:
     
-    	//////////
-    	// Static member functions
-    	static void 	bangMessCallback(void *data);
-    	static void 	loadMessCallback(void *data, t_symbol *path, t_floatarg nsamples);
+    //////////
+    // Static member functions
+    static void 	bangMessCallback(void *data);
+    static void 	loadMessCallback(void *data, t_symbol *path, t_floatarg nsamples);
 
-        // internal data
-        t_outlet        *m_dataout;
-        int             x_classify;
+    // internal data
+    t_outlet        *m_dataout;
+    int             x_classify;
 
-         // open cv classifier data
-        char            *x_filepath;
-        int             x_nsamples;
-        int             x_rsamples;
-        CvMat           *trainData;
-        CvMat           *trainClasses;
-        CvMat           *x_nearest;
-        CvMat           *x_dist;
-        int             x_pwidth;
-        int             x_pheight;
-        CvKNearest      *knn;
-        
-        std::string localPath;
+    // open cv classifier data
+    char            *x_filepath;
+    int             x_nsamples;
+    int             x_rsamples;
+    cv::Mat         trainData;
+    cv::Mat         trainClasses;
+    cv::Mat         x_nearest;
+    cv::Mat         x_dist;
+    int             x_pwidth;
+    int             x_pheight;
 
-	// The output and temporary images
-    	IplImage 	*rgba, *rgb, *grey;
-	
+    std::string localPath;
+
+    cv::Ptr<cv::ml::KNearest> x_knn{};
 };
-
-#endif	// for header file
